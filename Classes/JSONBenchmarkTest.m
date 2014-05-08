c8f93d2c3b2feaef20716538a2e202d382ffebfa
Daniel Demiss <dde@starfinanz.de>
2014-05-08 12:25:25 +0200

    ARCed the setup helper, and made it compile on iOS >=5 SDKs.

--- Classes/JSONBenchmarkTest.m
+++ Classes/JSONBenchmarkTest.m
@@ -7,36 +7,17 @@
 //
 
 #import "JSONBenchmarkTest.h"
-#import "JSONParser.h"
-#import "JSONWriter.h"
-#import "CJSONDeserializer.h"
+
 @implementation JSONBenchmarkTest
 
 @synthesize JSONData;
-
-- (void) dealloc
-{
-	[JSONData release];
-	JSONData = nil;
-	[super dealloc];
-}
-
 
 - (void)prepareData
 {
 	NSError *error = nil;
 	
-//	NSStringEncoding stringEncoding = NSUTF8StringEncoding;
-//	NSStringEncoding dataEncoding = stringEncoding; // NSUTF32BigEndianStringEncoding;	
-//	
-//	NSString *jsonString = [NSString stringWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"twitter_public_timeline" ofType:@"json"] encoding:stringEncoding error:nil];
-//	NSData *jsonData = [jsonString dataUsingEncoding:dataEncoding];
-//	self.JSONData = jsonData;
-//	self.collection = (NSArray *)[[CJSONDeserializer deserializer] deserialize:jsonData error:nil];
-	
-	self.JSONData = [JSON dataWithObject:self.collection options:0 encoding:NSUTF8StringEncoding error:NULL];
-	
-	
+	self.JSONData = [NSJSONSerialization dataWithJSONObject:self.collection options:0 error:NULL];
+
 	NSAssert1(error == nil, @"JSONBenchmarkTest - prepareData: error serializing data: %@", error);
 }
 
